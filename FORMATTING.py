e93ce9b37374ed2d2370995ca2266baf127dda76
Viggo <vtjensen@outlook.com>
2017-03-08 17:17:44 +0000

    Format update
    grid, row

---
+++ FORMATTING.py
@@ -0,0 +1,8 @@
+""" VIGGO: """
+
+"""
+grid = [row1, row2, row3, row4]
+row = [left-most-space, central-left-space, central-right-space, right-most-space]
+eg: grid = [[0, 2, 0, 4][4, 8, 0, 16][0, 0, 0, 1][4, 8, 2, 32]]
+
+"""
